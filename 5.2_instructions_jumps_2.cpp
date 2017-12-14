43b61e1d22ba798d443cbbb1b6e5af9d56ca7d7b
naplava <tomas.naplava@avast.com>
2017-12-14 10:44:17 +0100

    daz 5

---
+++ 5.2_instructions_jumps_2.cpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+int main(){
+	std::ifstream myfile;
+	myfile.open("c:\\Users\\naplava\\Desktop\\input.txt");
+	std::string line;
+	int jumps = 0;
+	std::vector<int> instructions;
+	while(std::getline(myfile, line)){
+		instructions.push_back(atoi(line.c_str()));
+	}
+
+	int ptr = 0;
+	while(ptr >= 0 && ptr < instructions.size()){
+		int offset = instructions[ptr];
+		int newPtr = ptr + offset;
+		if(offset >= 3){
+			instructions[ptr]--;
+		}
+		else{
+			instructions[ptr]++;
+		}
+		ptr = newPtr;
+		jumps++;
+	}
+
+	printf("jumps count is: %d\n", jumps);
+	getchar();
+
+    return 0;
+}
+
